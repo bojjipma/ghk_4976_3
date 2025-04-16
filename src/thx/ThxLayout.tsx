d5b73c30f239520d4a3f6627d020eef7defb7eff
u_m26tc <osolovyev@alfabank.ru>
2025-04-16 22:24:36 +0300

    commit

--- src/thx/ThxLayout.tsx
+++ src/thx/ThxLayout.tsx
@@ -4,17 +4,17 @@ import { thxSt } from './style.css';
 
 export const ThxLayout = () => {
   return (
-    <>
-      <div className={thxSt.container}>
-        <img src={heart} width={280} height={280} className={thxSt.rocket} />
-        <Typography.TitleResponsive style={{ margin: '24px 0 12px' }} font="system" tag="h1" view="medium" weight="semibold">
-          Сервис ещё в работе
-        </Typography.TitleResponsive>
-        <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-          Мы выбираем лучший подход, чтобы вам точно понравилось. С нетерпением ждём, когда всё заработает, чтобы вам
-          показать.
-        </Typography.Text>
-      </div>
-    </>
+      <>
+        <div className={thxSt.container}>
+          <img src={heart} width={280} height={280} className={thxSt.rocket} />
+          <Typography.TitleResponsive style={{ margin: '24px 0 12px' }} font="system" tag="h1" view="medium" weight="semibold">
+            Alfa Only
+          </Typography.TitleResponsive>
+          <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
+            Для подключения свяжитесь
+            с персональным менеджером
+          </Typography.Text>
+        </div>
+      </>
   );
 };
